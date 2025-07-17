892eaf76a8f4b6929259192b79f3a3ef825c9c88
Dangingxiong <1030227026@qq.com>
2025-07-17 10:12:57 +0800

    init

---
+++ api-gateway-test/gateway-provider-b/src/main/java/com/bank/test/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.bank.test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/test")
+    public String helloApiGateway() {
+        return "Hello Api-Gateway! Here is Provider-B!";
+    }
+}
